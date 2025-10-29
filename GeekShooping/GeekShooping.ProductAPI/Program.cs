2ad603524255f0842b054f8a5159e5dd801cd1be
rafael-sperandio <rafaelsperandio2001@gmail.com>
2025-10-29 20:16:11 -0300

    AddAuthentication para  GeekShopping.ProductAPI authentication  para geek_shopping e authenticação Swagger AddSecurityDefinition

--- GeekShooping/GeekShooping.ProductAPI/Program.cs
+++ GeekShooping/GeekShooping.ProductAPI/Program.cs
@@ -3,6 +3,7 @@ using GeekShooping.ProductAPI.Config.MappingConfig;
 using GeekShooping.ProductAPI.Model.Context;
 using GeekShooping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace GeekShooping.ProductAPI
@@ -14,6 +15,7 @@ namespace GeekShooping.ProductAPI
             var builder = WebApplication.CreateBuilder(args);
             //("MySQLConection:MySQLConnectionString"
             //builder.Configuration.GetConnectionString("DefaultConnection");
+
 
 
             var connection = builder.Configuration.GetConnectionString("MySQLConnection");
@@ -29,11 +31,63 @@ namespace GeekShooping.ProductAPI
             builder.Services.AddScoped< IProdructRepository, ProdructRepository>();
 
             builder.Services.AddControllers();
+
+
+            builder.Services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "https://localhost:4435/";//TODO possivel deixar no launch Settings
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+
+            builder.Services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiScope", policy =>
+                {
+                    policy.RequireAuthenticatedUser();//requisita usuario authenticado
+                    policy.RequireClaim("scope", "geek_shopping");
+                });
+            });
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
+
+
+
             builder.Services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" });
+                c.EnableAnnotations();
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"Enter 'Bearer' [space] and your token!",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",//
+                            Name = "Bearer",
+                            In= ParameterLocation.Header
+                        },
+                        new List<string> ()
+                    }
+                 });
             });
 
             var app = builder.Build();
@@ -46,6 +100,7 @@ namespace GeekShooping.ProductAPI
             }
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
